15d525717e9b0b51d361be8260d43199f255369f
Peter Mikkelsen <peterphmikkelsen@gmail.com>
2025-11-27 16:47:25 +0100

    🔒️ set data collection to none for firefox

--- wxt.config.ts
+++ wxt.config.ts
@@ -1,14 +1,21 @@
 import { defineConfig } from "wxt";
 import tailwindcss from "@tailwindcss/vite";
-import { nodePolyfills } from "vite-plugin-node-polyfills"
+import { nodePolyfills } from "vite-plugin-node-polyfills";
 
-// See https://wxt.dev/api/config.html
 export default defineConfig({
   modules: ["@wxt-dev/module-react"],
   vite: () => ({
-    plugins: [tailwindcss(), nodePolyfills()]
+    plugins: [tailwindcss(), nodePolyfills()],
   }),
   manifest: {
     permissions: ["storage", "tabs"],
+    browser_specific_settings: {
+      gecko: {
+        // @ts-ignore
+        data_collection_permissions: {
+          required: ["none"],
+        },
+      },
+    },
   },
 });
